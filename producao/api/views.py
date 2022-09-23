6a6195087f8fb4ed347d54f6150fc5f608a3ad0d
elastictek <miguel.jacinto@elastictek.com>
2022-09-23 14:43:44 +0100

    OFs & Current Settings

--- producao/api/views.py
+++ producao/api/views.py
@@ -5465,10 +5465,10 @@ def ValidarBobinagem(request, format=None):
                                             b[x]=0
                             else:
                                 b[x]=0
+                        bobine_values = {**{key: v[key] for key in v if key in columns}, **b} 
                         print("VALIDATING-------------------")
                         print(bobine_values['buracos_pos'])
                         print("END VALIDATING")
-                        bobine_values = {**{key: v[key] for key in v if key in columns}, **b} 
                         bobine_values['ff_m_ini'] = bobine_values['ff_pos'][0]['min'] if bobine_values['ff_pos'] is not None and len(bobine_values['ff_pos'])>0 else None
                         bobine_values['ff_m_fim'] = bobine_values['ff_pos'][0]['max'] if bobine_values['ff_pos'] is not None and len(bobine_values['ff_pos'])>0 else None
                         bobine_values['fc_diam_ini'] = bobine_values['fc_pos'][0]['min'] if bobine_values['fc_pos'] is not None and len(bobine_values['fc_pos'])>0 else None
